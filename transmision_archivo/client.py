2f1aa667e6d237b381420c3aeda7d0c649afb0f4
afdaza27 <af.daza@uniandes.edu.co>
2020-10-27 22:21:34 -0500

    babato cringe

--- transmision_archivo/client.py
+++ transmision_archivo/client.py
@@ -1,34 +1,61 @@
 import socket
+import threading
+from datetime import datetime
+import os
+import time
 
 
-message_from_client = "yo"
+def run_cliente(id):
+    now = datetime.now()
+    current_time = now.strftime("%H-%M-%S")
+    log_file = open("./logs/client/log-client-" +
+                    str(id)+"-"+current_time+".txt", "w")
+    log_file.write("Se inicia la descarga del archivo a las " +
+                   current_time+"\n")
+    print("running client "+str(id))
+    message_from_client = "yo"
+    bytes_to_send = str.encode(message_from_client)
+    server_address_port = ("127.0.0.1", 20001)
+    buffer_size = 8192
+    destination_file_path = "./vids/recieve-"+str(id)+"-"+current_time+".mp4"
+    f = open(destination_file_path, "wb")
+    log_file.write("Se guardara el archivo en "+destination_file_path+"\n")
+    # create a UDP socket at client side
+    udp_client_socket = socket.socket(
+        family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    # udp_client_socket.bind(server_address_port)
+    # send to server using created udp socket
+    udp_client_socket.sendto(bytes_to_send, server_address_port)
+    hash_sent, addr = udp_client_socket.recvfrom(buffer_size)
+    data, addr = udp_client_socket.recvfrom(buffer_size)
+    start_time = time.time()
+    n = 0
+    try:
+        while data:
+            n += 1
+            f.write(data)
+            udp_client_socket.settimeout(2)
+            data, addr = udp_client_socket.recvfrom(buffer_size)
+    except:
+        if str(hash(f)) != hash_sent.decode("utf-8"):
+            log_file.write("El hash del archivo enviado ("+hash_sent.decode("utf-8") +
+                           ") no corresponde al del archivo descargado ("+str(hash(f))+"). Por lo tanto, el archivo recibido esta incompleto.\n")
+        else:
+            log_file.write("El archivo fue descargado con integridad.\n")
+        f.close()
+        udp_client_socket.close()
+        print("file downloaded")
+        end_time = time.time()
+        st = os.stat(destination_file_path)
+        log_file.write("El tamaño del archivo es de " +
+                       str(st.st_size)+" Bytes\n")
+        log_file.write("El archivo se descargo con exito en un tiempo de " +
+                       str(round(end_time-start_time, 5))+" segundos\n")
+        log_file.write("Total de segmentos descargados: " +
+                       str(n)+" segmentos\n")
 
-bytes_to_send = str.encode(message_from_client)
 
-server_address_port = ("127.0.0.1", 20001)
-
-buffer_size = 1024
-
-file = open("recieve.txt", "wb")
-
-#create a UDP socket at client side
-
-udp_client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-#udp_client_socket.bind(server_address_port)
-
-
-# send to server using created udp socket
-
-udp_client_socket.sendto(bytes_to_send, server_address_port)
-
-data, addr = udp_client_socket.recvfrom(buffer_size)
-
-try:
-    while data:
-        file.write(data)
-        udp_client_socket.settimeout(2)
-        data, addr = udp_client_socket.recvfrom(buffer_size)
-except:
-    file.close()
-    udp_client_socket.close()
-    print("file downloaded")
+clientes = int(input("Ingrese la cantidad de clientes:\n"))
+for i in range(clientes):
+    cliente = threading.Thread(target=run_cliente(i))
+    cliente.start()
--- transmision_archivo/server.py
+++ transmision_archivo/server.py
@@ -1,20 +1,71 @@
 import socket
 import sys
+import threading
+import time
+from datetime import datetime
+import os
+
+
+def archivoLmao(nombre,  socket,  clientes):
+    for i in range(clientes):
+        servidorDelegado = threading.Thread(
+            target=atenderCliente(nombre, socket, i))
+        servidorDelegado.start()
+
+
+def atenderCliente(file_name, socket, id):
+    print("Atendiendo cliente "+str(id)+"...")
+    st = os.stat(file_name)
+    now = datetime.now()
+    current_time = now.strftime("%H-%M-%S")
+    log_file = open("./logs/server/log-servidor-delegado-" +
+                    str(id)+"-"+current_time+".txt", "w")
+    log_file.write("Se inicia el envio del archivo a las "+current_time+"\n")
+    log_file.write("Se enviara el archivo "+file_name+"\n")
+    f = open(file_name, "rb")
+
+    log_file.write("El tamaño del archivo es de "+str(st.st_size)+" Bytes\n")
+    data = f.read(buffersize)
+    bytes_address_pair = udp_server_socket.recvfrom(buffersize)
+    hash_string = str(hash(f))
+    hash_bytes = str.encode(hash_string)
+
+    message = bytes_address_pair[0]
+
+    ip_address = bytes_address_pair[1]
+    udp_server_socket.sendto(hash_bytes, ip_address)
+    client_message = "Message from client:{}".format(message)
+    client_ip = "Client ip address:{}".format(ip_address)
+    print("message from client: " + client_message)
+    print("client address: " + client_ip)
+    start_time = time.time()
+    n = 0
+
+    # udp_server_socket.sendto(data, ip_address)
+    while data:
+        n += 1
+        if udp_server_socket.sendto(data, ip_address):
+            print("sending...")
+            data = f.read(buffersize)
+    end_time = time.time()
+    log_file.write("El archivo se envio con exito en un tiempo de " +
+                   str(round(end_time-start_time, 5))+" segundos\n")
+    log_file.write("Total de segmentos enviados: "+str(n)+" segmentos\n")
+    f.close()
+
 
 localip = "0.0.0.0"
 
 localport = 20001
 
-buffersize = 1024
+buffersize = 8192
 address = (localip, localport)
-
-file_name = "test.txt"
-
 
 
 # create a datagram socket
 
-udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+udp_server_socket = socket.socket(
+    family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 # bind to address and ip
 
@@ -23,23 +74,33 @@ udp_server_socket.bind((localip, localport))
 print("udp server up and listening")
 
 # listen for incoming datagrams
-
-while (True):
-    file = open(file_name, "rb")
-    data = file.read(buffersize)
-    bytes_address_pair = udp_server_socket.recvfrom(buffersize)
-    message = bytes_address_pair[0]
-
-    ip_address = bytes_address_pair[1]
-
-    client_message = "Message from client:{}".format(message)
-    client_ip = "Client ip address:{}".format(ip_address)
-    print("message from client: " + client_message)
-    print("client address: " + client_ip)
-
-    #udp_server_socket.sendto(data, ip_address)
-    while data:
-        if udp_server_socket.sendto(data, ip_address):
-            print("sending...")
-            data = file.read(buffersize)
-    file.close()
+while True:
+    ejecutado = False
+    while not ejecutado:
+        print("-------------------------------------------------------")
+        print("Bienvenido a TuServidorUDP")
+        print("¿Qué deseas hacer?")
+        print("1. Ver archivos disponibles")
+        print("2. Enviar archivos")
+        print("-------------------------------------------------------")
+        risposta = int(input(""))
+        if risposta == 1:
+            print("1. video.mp4")
+            print("2. documental.mp4")
+        elif risposta == 2:
+            print("¿Qué archivo desea enviar?")
+            print("-------------------------------------------------------")
+            print("1. video.mp4 (100MB)")
+            print("2. documental.mp4 (200MB)")
+            print("-------------------------------------------------------")
+            archivo = input("")
+            clientes = int(
+                input("Ingrese la cantidad de clientes a enviar el archivo\n"))
+            if archivo == "1":
+                archivoLmao("video.mp4", udp_server_socket, clientes)
+                ejecutado = True
+            elif archivo == "2":
+                archivoLmao("documental.mp4", udp_server_socket, clientes)
+                ejecutado = True
+        else:
+            print("Ingrese un valor válido, no es tan dificil lmao")
